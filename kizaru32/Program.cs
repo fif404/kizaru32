bcbe5f12babac50f96e1e9affc6884b101063768
fif <fif@DESKTOP-BFHUIKE>
2022-11-03 09:51:35 +1200

    Добавьте файлы проекта.

---
+++ kizaru32/Program.cs
@@ -0,0 +1,9 @@
+﻿// Скрипников Сергей 17 вариант 2.2
+Console.WriteLine("Введите число x:");
+double x = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите число y:");
+double y = double.Parse(Console.ReadLine());
+if ((x >= 0 ) && (y <=0))
+        Console.WriteLine("данная точка лежит в четвёртой четверти");
+else
+    Console.WriteLine("данная точка не лежит в четвёртой четверти");
